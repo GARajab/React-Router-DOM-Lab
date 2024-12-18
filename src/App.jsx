04a6027903189a4be55c70f89a5ed7b1f35c46ce
GARajab <mohamed.rajab@ewa.com>
2024-12-19 01:25:59 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react"
-
+import React, { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import NavBar from "./components/NavBar"
+import MailboxForm from "./components/MailboxForm"
+import MailboxList from "./components/MailboxList"
+import MailboxDetails from "./components/MailboxDetails"
 import "./App.css"
-
 const App = () => {
-  return <></>
+  const [mailboxes, setMailboxes] = useState([])
+
+  const addBox = (newMailbox) => {
+    const newId = mailboxes.length ? mailboxes[mailboxes.length - 1]._id + 1 : 1
+    setMailboxes([...mailboxes, { ...newMailbox, _id: newId }])
+  }
+
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <main>
+                <h1>Post Office</h1>
+              </main>
+            }
+          />
+          <Route
+            path="/mailboxes"
+            element={<MailboxList mailboxes={mailboxes} />}
+          />
+          <Route
+            path="/new-mailbox"
+            element={<MailboxForm addBox={addBox} />}
+          />
+          <Route
+            path="/mailboxes/:mailboxId"
+            element={<MailboxDetails mailboxes={mailboxes} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
